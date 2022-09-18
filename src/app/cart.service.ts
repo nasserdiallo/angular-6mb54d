5be62e3a291aa2b891c12e59c1323b8d20e2396d
nasserdiallo <67609106+nasserdiallo@users.noreply.github.com>
2022-09-18 15:04:11 +0000

    2 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Product } from './products';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
   items: Product[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  
   addToCart(product: Product) {
     this.items.push(product);
   }
@@ -21,5 +22,11 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 }
