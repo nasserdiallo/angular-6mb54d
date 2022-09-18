5e31f0cd1f218d6f660285fa3e72b871160d9d15
nasserdiallo <67609106+nasserdiallo@users.noreply.github.com>
2022-09-18 14:48:46 +0000

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   imports: [
     BrowserModule,
